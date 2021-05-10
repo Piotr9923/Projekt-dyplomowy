4a7837489c2ca9d7c8d6a6944087617872187c13
Your Name <p-rzewnicki@o2.pl>
2021-05-10 08:37:28 +0200

    Dodano przyciski wyboru trybu rejestracji

--- frontend/src/public/registration/Registration.js
+++ frontend/src/public/registration/Registration.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import PublicHeader from '../PublicHeader';
+import Button from 'react-bootstrap/Button'
+import HelpIcon from '@material-ui/icons/Help';
 
 class RegistrationTypePage extends Component{
 
+
+    showInfoNewButton(){
+        alert("Wybierz tą opcję jeśli jesteś naszym nowym klientem!");
+    }
+
+    showInfoClientButton(){
+        alert("Wybierz tą opcję jeśli powierzyłeś nam do naprawy swój sprzęt w serwisie!");
+    }
 
     render(){
 
@@ -14,8 +24,8 @@ class RegistrationTypePage extends Component{
                 <PublicHeader />
 
                 <h2 style={{display: "flex","justify-content":"center","align-items":"center"}}>Wybierz sposób rejestracji:</h2><br/>
-                <Link to="/registration/new">Nowy użytkownik</Link> - wybierz tą opcję jeśli jesteś naszym nowym klientem!<br/><br/>
-                <Link to="/registration/client">Klient serwisu</Link> - wybierz tą opcję jeśli powierzyłeś nam do naprawy swój sprzęt w serwisie!
+                <div style={{display: "table",margin:"0 auto"}}><Link to={"registration/new"}><Button  variant="secondary" size="lg">Nowy użytkownik</Button></Link><HelpIcon color="action" onClick={this.showInfoNewButton}/><br/><br/></div>
+                <div style={{display: "table",margin:"0 auto"}}><Link to={"registration/client"}><Button  variant="secondary" size="lg">Klient serwisu</Button></Link><HelpIcon color="action" onClick={this.showInfoClientButton}/><br/><br/></div>
 
             </div>
         )
